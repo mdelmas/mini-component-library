6a090b3a5bd12a5c27009dfa98bef56a6162a868
Morgane Delmas <mdelmas93@gmail.com>
2024-01-15 23:49:16 +0100

    Part 1 : progress bar

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -1,12 +1,59 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { COLORS } from '../../constants';
-import VisuallyHidden from '../VisuallyHidden';
+import { COLORS } from "../../constants";
+import VisuallyHidden from "../VisuallyHidden";
+
+const SIZES = {
+  large: {
+    height: "24px",
+    "border-radius": "8px",
+    padding: "4px",
+  },
+  medium: {
+    height: "12px",
+    "border-radius": "4px",
+  },
+  small: {
+    height: "8px",
+    "border-radius": "4px",
+  },
+};
 
 const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+  return (
+    <ProgressBarWrapper style={SIZES[size]}>
+      <ProgressIndicatorWrapper>
+        <ProgressIndicator
+          style={{ "--progress": value + "%" }}
+        ></ProgressIndicator>
+      </ProgressIndicatorWrapper>
+    </ProgressBarWrapper>
+  );
 };
+
+const ProgressBarWrapper = styled.div`
+  background-color: ${COLORS.transparentGray15};
+  width: 100%;
+
+  box-shadow: 0px 2px 4px 0px ${COLORS.transparentGray35} inset;
+`;
+/* min-width: 300px;
+  width: 100%; */
+
+const ProgressIndicatorWrapper = styled.div`
+  overflow: clip;
+  border-radius: 4px;
+
+  height: 100%;
+  width: 100%;
+`;
+
+const ProgressIndicator = styled.div`
+  background-color: ${COLORS.primary};
+  width: var(--progress);
+  height: 100%;
+`;
 
 export default ProgressBar;
